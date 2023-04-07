a1c1d919af22802615e2d15eaea7a4e92bdc019e
loyispa <litiegen456250@gmail.com>
2023-04-07 19:34:29 +0800

    fix mod export issue

--- src/lib.rs
+++ src/lib.rs
@@ -61,12 +61,12 @@ Decode from bytes:
  */
 
 mod error;
-mod index_mapping;
+pub mod index_mapping;
 mod input;
 mod output;
 mod serde;
 mod sketch;
-mod store;
+pub mod store;
 
 pub use self::error::Error;
 pub use self::index_mapping::CubicallyInterpolatedMapping;
